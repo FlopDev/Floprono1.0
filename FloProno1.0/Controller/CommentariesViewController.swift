29b37940a5b8d13c098979a9c47e22cb2844463a
Florian Peyrony <peyronyflorian.pro@gmail.com>
2022-09-27 09:46:57 +0200

    working on finishing the app flow and starting research on model

---
+++ FloProno1.0/Controller/CommentariesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CommentariesViewController.swift
+//  FloProno1.0
+//
+//  Created by Florian Peyrony on 13/09/2022.
+//  Copyright © 2022 Florian Peyrony. All rights reserved.
+//
+
+import UIKit
+
+class CommentariesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FloProno1.0/Model/Commentary.swift
@@ -0,0 +1,9 @@
+//
+//  Commentary.swift
+//  FloProno1.0
+//
+//  Created by Florian Peyrony on 13/09/2022.
+//  Copyright © 2022 Florian Peyrony. All rights reserved.
+//
+
+import Foundation
